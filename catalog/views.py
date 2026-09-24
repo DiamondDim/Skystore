aa1708322ec0da2312d814e5cf154f937ac5783a
Дмитрий <mecheny91@yandex.ru>
2026-09-24 05:33:53 +0300

    feat: implement home, catalog, and contacts pages with Bootstrap layout

--- catalog/views.py
+++ catalog/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 
-def index(request):
-    return HttpResponse("Это страница каталога товаров (catalog).")
+def home(request):
+    return render(request, 'catalog/home.html')
 
+def contacts(request):
+    return render(request, 'catalog/contacts.html')
